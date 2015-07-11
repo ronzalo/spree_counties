68a47348f3e723afa37420cf2837e3a070081ce0
Marcelo Espina <mespina@acid.cl>
2015-07-10 21:45:34 -0300

    add counties menu item

---
+++ app/overrides/spree/admin/admin/shared/sub_menu/add_county_menu_item.rb
@@ -0,0 +1,5 @@
+Deface::Override.new virtual_path:  "spree/admin/shared/sub_menu/_configuration",
+                     name:          "add_counties_form",
+                     original:      "0334d9c034bbd6d42b2d9da5a2f98719f9a68e4c",
+                     insert_before: "erb:contains('admin_payment_methods_path')",
+                     partial:       "spree/admin/counties/county_menu_item"
