76ef525dae2b776e3da9ce76670d8b3a85571518
Javier Espinoza <javierieh@acid.cl>
2016-05-20 16:02:49 -0400

    Add shoulda config on spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -90,3 +90,10 @@ RSpec.configure do |config|
   config.fail_fast = ENV['FAIL_FAST'] || false
   config.order = "random"
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
